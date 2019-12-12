b036f515f0bda9df50078370e932946511a9135f
kaebit <kaibee@kakao.com>
2019-12-12 23:38:11 +0900

    Initial commit

---
+++ src/test/java/com/kaebit/validation/ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaebit.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
